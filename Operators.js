a6b592e7bf15dbe144be8903be9edc3964a6abe6
Tanya Aggarwal <tanya.aggarwal106@gmail.com>
2025-08-10 20:07:52 +0530

    Operators

---
+++ Operators.js
@@ -0,0 +1,11 @@
+//Nullish Operator- used to skip null or undefined values
+let val1=50??100;
+console.log(val1);
+let val2=null??100;
+console.log(val2);
+let val3=undefined??1100;
+console.log(val3);
+
+//Ternary Operator
+const num=100;
+num>10 ? console.log("Greater") : console.log("Smaller");
